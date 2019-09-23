994cf0d419880a8e1fb18d2f30b95786daef0aaa
Daria Snow <dariasnow97@gmail.com>
2019-09-23 23:20:50 +0300

    Remake calculator in accordance with PEP-8. Try to build Travis.

--- easy_calc.py
+++ easy_calc.py
@@ -1,11 +1,47 @@
-a = float(input())
-oper = str(input())
-b = float(input())
+def is_float(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
 
-if oper == '/' and b == 0:
-    print("Oooops!")
-elif oper in ('+', '-', '/', '*'):
-	com = str(a) + oper + str(b)
-	print(eval(com))
-elif oper == '^':
-  print(a**b)
+
+i = 0
+print("Hi! Welcome to Easy Calculator!")
+while i == 0:
+    print("Please, enter one of these operations: +, -, *, /, ^ or 'exit' for exit:")
+    operation = input()
+    if operation == 'exit':
+        i = 1
+        print("Goodbye!")
+        break
+    else:
+        print("Enter the first number:")
+        a = input()
+        if is_float(a):
+            a = float(a)
+        else:
+            print("Incorrect number!")
+            continue
+        print("Enter the second number:")
+        b = input()
+        if is_float(b):
+            b = float(b)
+        else:
+            print("Incorrect number!")
+            continue
+
+    if operation == '/' and b == 0:
+        print("Oooops! Divide-by-zero isn't supported by me:( Try other numbers or operation!")
+    elif operation == '+':
+        print(a+b)
+    elif operation == '-':
+        print(a-b)
+    elif operation == '*':
+        print(a*b)
+    elif operation == '/':
+        print(a/b)
+    elif operation == '^':
+        print(a**b)
+    else:
+        print("This operation isn't supported by me:( Try other")
