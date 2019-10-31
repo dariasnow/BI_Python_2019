ec3099185bcc76841102ac8b3b5dc5562cc21d4c
Daria Snow <dariasnow97@gmail.com>
2019-10-31 21:53:02 +0300

    add a new solution without itertools

---
+++ hw04_group_equal_without_itertools.py
@@ -0,0 +1,16 @@
+
+def group_equal(els):
+    final_list = []
+    for (i, x) in enumerate(els):
+        if i < 1 or not isinstance(x, type(els[i - 1])) or x != els[i - 1]:
+            final_list.append([x])
+        else:
+            final_list[-1].append(x)
+    return final_list
+
+
+if __name__ == '__main__':
+    assert group_equal([1, 1, 4, 4, 4, "hello", "hello", 4]) == [[1, 1], [4, 4, 4], ["hello", "hello"], [4]]
+    assert group_equal([1, 2, 3, 4]) == [[1], [2], [3], [4]]
+    assert group_equal([1]) == [[1]]
+    assert group_equal([]) == []
